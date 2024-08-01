1d9d58fdc695b842346c8c47aaa6379badd19422
Shubhangi786 <shubhangimsable@gmail.com>
2024-08-01 14:52:09 +0530

    Playwright task 1

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  page.setViewportSize({ width: 1600, height: 1200 });
+  await page.goto('https://www.google.com/');
+  await page.getByLabel('Search', { exact: true }).click();
+  await page.getByLabel('Search', { exact: true }).fill('Playwright');
+  await page.goto('https://playwright.dev/');
+});
